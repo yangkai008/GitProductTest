c865423960e5afbdfee2be8401d06ebb0882cc97
yangkaii <1179695869@qq.com>
2018-10-11 16:57:10 +0800

    nsid

--- GitProduct/src/GitTest/test.java
+++ GitProduct/src/GitTest/test.java
@@ -2,6 +2,6 @@ package GitTest;
 
 public class test {
 	private String name="asdasd";
-	private String name1="yangkai";
-	private String name2="yanghui";
+	private String name1="yangkai001";
+	private String name2="yanghui001";
 }
